e4dc0db36cae5b471f88df26c25d1144d43f825f
uma1796 <umamaheswari1398@gmail.com>
2021-02-19 14:44:04 +0530

    first process done

---
+++ src/main/java/com/testng/concepts/Simple_annotion.java
@@ -0,0 +1,24 @@
+package com.testng.concepts;
+
+import org.testng.annotations.Test;
+
+public class Simple_annotion {
+	
+	
+	 private void Browserlaunch() {
+      System.out.println("Browserlaunch");
+	}
+	@Test
+	//private void mobile search() we can also give as
+	 public static void MoblieSearch() {
+		 System.out.println("Mobile search");
+
+	 }
+	private void laptopsearch() {
+System.out.println("Laptop ");
+	}
+	 
+
+	
+
+}
